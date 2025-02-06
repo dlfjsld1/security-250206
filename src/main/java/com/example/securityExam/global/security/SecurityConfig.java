8477023f84dc4e3af9947424016d32a0f329e871
dlfjsld1 <ltk900216@gmail.com>
2025-02-06 15:36:55 +0900

    h2-console setting

--- src/main/java/com/example/securityExam/global/security/SecurityConfig.java
+++ src/main/java/com/example/securityExam/global/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.securityExam.global.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -13,11 +16,17 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
-                                .requestMatchers(HttpMethod.GET, "/api/*/posts/{id:\\d+}", "/api/*/posts", "/api/*/posts/{postId:\\d+}/comments")
+                                .requestMatchers("/h2-console/**")
                                 .permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/*/posts/{id:\\d+}", "/api/*/posts", "/api/*/posts/{postId:\\d+}/comments")
+                                .permitAll() //이 위에 지정한건 허용하고
                                 .anyRequest()
-                                .authenticated()
+                                .authenticated() // 나머지는 막아라
                 )
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .csrf((csrf) -> csrf.disable())
         ;
         return http.build();
     }
