8718afe06092c76912116e3b770f7ad0a63105d4
dlfjsld1 <ltk900216@gmail.com>
2025-02-07 09:25:25 +0900

    annotation to CustomUserDetailService

--- src/main/java/com/example/securityExam/global/security/CustomUserDetailService.java
+++ src/main/java/com/example/securityExam/global/security/CustomUserDetailService.java
@@ -11,6 +11,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+//이 코드의 역할은 스프링 시큐리티가 제공하는 UserDetailsService 인터페이스를 구현하여,
+// 사용자 정보를 데이터베이스에서 가져오는 로직을 정의하는 것.
+// CustomUserDetailService의 역할은 사용자 이름(username)을 기반으로
+// 데이터베이스에서 사용자 정보를 조회하고,
+// 그 정보를 스프링 시큐리티가 이해할 수 있는 UserDetails 객체로 변환하여 반환하는 것.
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
